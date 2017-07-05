593e5251015e0b1aaf119cae8997f55108dafa41
hakiemaul <noersedya@gmail.com>
2017-07-05 16:33:21 +0700

    tidying trash line

--- free/src/components/WeatherContent.js
+++ free/src/components/WeatherContent.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios'
 import { connect } from 'react-redux'
 
 import { getCurrentWeather, getForecasts, findCityCurrent, findCityForecasts } from '../actions'
